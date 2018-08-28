fde070227d708bbb139a0a2fde5f595248ab7c4b
wangjiaojiao123 <3197612935@qq.com>
2018-08-28 14:56:12 +0800

    添加axios插件

---
+++ dist/precache-manifest.87c3ca835652977e93022b4d8b0e172f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0eaa66aec9c70667468e",
+    "url": "/js/chunk-vendors.dd398368.js"
+  },
+  {
+    "revision": "e687e729b0079813ac3d",
+    "url": "/js/app.ac07c65f.js"
+  },
+  {
+    "revision": "083b480c704835c0eac2",
+    "url": "/js/about.5d688836.js"
+  },
+  {
+    "revision": "6a799f498e67e7437ac3de2a2a167468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e687e729b0079813ac3d",
+    "url": "/css/app.7e867870.css"
+  }
+];
